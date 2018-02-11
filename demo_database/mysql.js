5790c106e3a966b33611e2d45afccb33e3da0ae3
wangying <909359916@qq.com>
2018-02-11 11:16:16 +0800

    test

--- demo_database/mysql.js
+++ demo_database/mysql.js
@@ -9,3 +9,44 @@ var db = mysql.createConnection({
   password : 'fe246650',
   database : 'timetrack'
 })
+
+
+var server = http.createServer(function(req, res){
+  switch(req.method) {
+    case 'POST':
+      switch(req.url) {
+        case '/':
+          work.add(db, req, res);
+          break;
+        case '/archive':
+          work.archive(db, req,res);
+        case '/delete' : 
+          work.delete(db, req,res);
+      }
+      break;
+    case 'GET': 
+        switch(req.url) {
+          case '/' : 
+            work.show(db. res);
+            break;
+          case '/archived' : 
+            work.showArchived(db, res)
+        }
+        break;
+  }
+})
+
+db.query(
+    "CREATE TABLE IF NOT EXISTS WORK ("
+  + "id INIT(10) NOT NULL AUTO_INCREMENT,"
+  + "hours DECIMAL(5,2) DEFAULT 0,"
+  + "date DATE, "
+  + "archived INT(1) DEFAULT 0,"
+  + "description LONGTEXT, "
+  + "PRIMARY KEY(id), ",
+  function(err){
+    if(err) throw err;
+    console.log('server started ...')
+    server.listen(3000,'127.0.0.1');
+  }
+)
